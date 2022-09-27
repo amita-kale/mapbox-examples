0fd1dd6b2f1793015d6aef940d043dc98a4394fb
amita-kale <amitakale297@gmail.com>
2022-09-27 12:29:29 +0530

    Added navigation and full screen control and drag tool

--- components/mapboxexample.vue
+++ components/mapboxexample.vue
@@ -1,8 +1,10 @@
 <template>
 
-
     <head>
         <link href="https://api.mapbox.com/mapbox-gl-js/v2.10.0/mapbox-gl.css" rel="stylesheet">
+        <link rel='stylesheet'
+            href='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-draw/v1.2.0/mapbox-gl-draw.css'
+            type='text/css' />
     </head>
     <v-map class="w-full h-full" :options="state.map" @loaded="onMapLoaded"></v-map>
 
@@ -12,6 +14,8 @@
 <script setup lang="ts">
 import VMap from "v-mapbox";
 import mapboxgl from "mapbox-gl";
+import MapboxDraw from "@mapbox/mapbox-gl-draw";
+
 const state: any = reactive({
     map: {
         accessToken:
@@ -21,11 +25,38 @@ const state: any = reactive({
         center: [444.04931277036667, 26.266912177018096] as number[], //uses longitude, latitude
         zoom: 6,
         maxZoom: 22,
+        // projection: 'globe'
+
     }
 });
 
 async function onMapLoaded(map: mapboxgl.Map) {
     console.log("mathod call");
+    new mapboxgl.Marker({
+        color: "#" + (Math.random().toString(16) + "000000").substring(2, 8),
+        draggable: true
+    }).setLngLat([30.5, 50.5])
+        .addTo(map);
+
+    var Draw = new MapboxDraw();
+    map.addControl(Draw, 'top-right');
+
+    // map.on('click', (e) => {
+    //     console.log(e);
+    //     console.log(e.lngLat)
+    //     new mapboxgl.Marker({
+    //         draggable: true,
+    //         color: "#" + (Math.random().toString(16) + "000000").substring(2, 8),
+    //     }).setLngLat([e.lngLat.lng, e.lngLat.lat]).addTo(map)
+    //     new mapboxgl.Popup()
+    //         .setLngLat([e.lngLat.lng, e.lngLat.lat])
+    //         .setHTML("hello world")
+    //         .addTo(map);
+    // })
+
+
+    map.addControl(new mapboxgl.FullscreenControl());
+    map.addControl(new mapboxgl.NavigationControl(), 'top-right');
 }
 </script>
 <style scoped>
@@ -36,13 +67,7 @@ async function onMapLoaded(map: mapboxgl.Map) {
     width: 100%;
 }
 
-.w-screen {
-    width: 100vw;
-}
 
-.h-screen {
-    height: 100vh;
-}
 
 .h-full {
     height: 100%;
@@ -56,4 +81,5 @@ async function onMapLoaded(map: mapboxgl.Map) {
     max-width: 400px;
     font: 12px/20px 'Helvetica Neue', Arial, Helvetica, sans-serif;
 }
-</style>
+</style> 
+
---
+++ components/mapboxglobe.vue
@@ -0,0 +1,72 @@
+<template>
+
+
+    <head>
+        <link href="https://api.mapbox.com/mapbox-gl-js/v2.10.0/mapbox-gl.css" rel="stylesheet">
+    </head>
+    <v-map class="w-full h-full" :options="state.map" @loaded="onMapLoaded"></v-map>
+
+    <div id="map"></div>
+</template>
+
+<script setup lang="ts">
+import VMap from "v-mapbox";
+import mapboxgl from "mapbox-gl";
+mapboxgl.accessToken = "pk.eyJ1Ijoic29jaWFsZXhwbG9yZXIiLCJhIjoiREFQbXBISSJ9.dwFTwfSaWsHvktHrRtpydQ";
+const state: any = reactive({
+    map: {
+        // accessToken:
+        //     "pk.eyJ1Ijoic29jaWFsZXhwbG9yZXIiLCJhIjoiREFQbXBISSJ9.dwFTwfSaWsHvktHrRtpydQ",
+        // style: "mapbox://styles/mapbox/streets-v11?optimize=true",
+        // // style: "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json",
+        // center: [444.04931277036667, 26.266912177018096] as number[], //uses longitude, latitude
+        // zoom: 6,
+        // maxZoom: 22,
+        // projection: 'globe'
+        container: 'map',
+        style: 'mapbox://styles/mapbox/satellite-streets-v11',
+        zoom: 11,
+        center: [444.04931277036667, 26.266912177018096] as number[],
+        projection: 'globe'
+    }
+});
+
+async function onMapLoaded(map: mapboxgl.Map) {
+    console.log("mathod call");
+    map.on('load', () => {
+        // Set the default atmosphere style
+        map.setFog({});
+    });
+    // map.addControl(new mapboxgl.NavigationControl(), 'top-left');
+}
+</script>
+<style scoped>
+#map {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+}
+
+/* .w-screen {
+    width: 100vw;
+}
+
+.h-screen {
+    height: 100vh;
+} */
+
+.h-full {
+    height: 100%;
+}
+
+.w-full {
+    width: 100%;
+}
+
+.mapboxgl-popup {
+    max-width: 400px;
+    font: 12px/20px 'Helvetica Neue', Arial, Helvetica, sans-serif;
+}
+</style> 
+
