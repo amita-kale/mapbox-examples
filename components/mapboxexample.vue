757c39693b68ef4a7f485d7077b0995cb6fd4a1f
amita-kale <amitakale297@gmail.com>
2022-09-27 10:32:31 +0530

    feat: displayed basic map

--- components/mapboxexample.vue
+++ components/mapboxexample.vue
@@ -1,6 +1,59 @@
 <template>
-    <h1>Mapbox Ex</h1>
-</template>
-<script setup lang="ts">
 
+
+    <head>
+        <link href="https://api.mapbox.com/mapbox-gl-js/v2.10.0/mapbox-gl.css" rel="stylesheet">
+    </head>
+    <v-map class="w-full h-full" :options="state.map" @loaded="onMapLoaded"></v-map>
+
+    <div id="map"></div>
+</template>
+
+<script setup lang="ts">
+import VMap from "v-mapbox";
+import mapboxgl from "mapbox-gl";
+const state: any = reactive({
+    map: {
+        accessToken:
+            "pk.eyJ1Ijoic29jaWFsZXhwbG9yZXIiLCJhIjoiREFQbXBISSJ9.dwFTwfSaWsHvktHrRtpydQ",
+        style: "mapbox://styles/mapbox/streets-v11?optimize=true",
+        // style: "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json",
+        center: [444.04931277036667, 26.266912177018096] as number[], //uses longitude, latitude
+        zoom: 6,
+        maxZoom: 22,
+    }
+});
+
+async function onMapLoaded(map: mapboxgl.Map) {
+    console.log("mathod call");
+}
 </script>
+<style scoped>
+#map {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+}
+
+.w-screen {
+    width: 100vw;
+}
+
+.h-screen {
+    height: 100vh;
+}
+
+.h-full {
+    height: 100%;
+}
+
+.w-full {
+    width: 100%;
+}
+
+.mapboxgl-popup {
+    max-width: 400px;
+    font: 12px/20px 'Helvetica Neue', Arial, Helvetica, sans-serif;
+}
+</style>
